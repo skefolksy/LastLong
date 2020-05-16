4afc0a19e7a7a21be66c2201ce1a01dbdd58b00c
skefolksy <skefolksy@hotmail.com>
2020-05-17 03:40:10 +0700

    finish

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAhjk03i7FzZ83Z-Yf6I8Tv5YyHLWGgZhk",
+  authDomain: "lastlong-afc33.firebaseapp.com",
+  databaseURL: "https://lastlong-afc33.firebaseio.com",
+  projectId: "lastlong-afc33",
+  storageBucket: "lastlong-afc33.appspot.com",
+  messagingSenderId: "732348124902",
+  appId: "1:732348124902:web:897ce1fc37420bb1f57527",
+  measurementId: "G-QQH48J3RVZ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
